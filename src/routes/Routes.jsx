57c7981605bc36c74c8e9dc1a0ea8ac68673a047
fahimprito <ahmmedahmmed75@gmail.com>
2025-01-16 23:19:49 +0600

    firebase setup, errorpage

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,16 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayouts></MainLayouts>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: "/",
                 element: <Home></Home>,
-            }
+            },
+            {
+                path: "/login",
+                element: <Login></Login>,
+            },
+            {
+                path: "/register",
+                element: <Register></Register>,
+            },
         ]
     },
 ]);
