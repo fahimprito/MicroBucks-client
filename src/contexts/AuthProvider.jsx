6f583a40c12dd1e514b2434eec4eeaf0b556f732
fahimprito <ahmmedahmmed75@gmail.com>
2025-01-20 02:44:09 +0600

    store a secret access-token

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -3,12 +3,14 @@ import AuthContext from "./AuthContext";
 import { useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import auth from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -37,13 +39,28 @@ const AuthProvider = ({ children }) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('current User', currentUser);
             setUser(currentUser);
-            setLoading(false);
+            if (currentUser) {
+                // get token and store in local
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                            setLoading(false);
+                        }
+                    })
+            }
+            else {
+                // remove token
+                localStorage.removeItem('access-token');
+                setLoading(false);
+            }
         })
 
         return () => {
             unSubscribe();
         }
-    }, [])
+    }, [axiosPublic])
 
     const authInfo = {
         user,
